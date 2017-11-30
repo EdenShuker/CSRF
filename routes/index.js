5059b850b12ff7087600b5131f51bb7a4a85539f
Eden Shuker <edenshuker1997@gmail.com>
2017-11-30 14:55:02 +0200

    bank transfer form

--- routes/index.js
+++ routes/index.js
@@ -28,13 +28,28 @@ router.get('/login', function(req, res) {
     res.render('login', { user : req.user });
 });
 
+
 router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
+    res.redirect('/transfer');
 });
+
+
+router.get('/transfer', function(req, res) {
+    res.render('bankTransferForm', { user : req.user });
+});
+
+
+router.post('/transfer', function(req, res) {
+    console.log("amount: " + req.body.amount + ",  account number: " + req.body.accountNum);
+    res.send("Transfer executed")
+});
+
 
 router.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
 });
+
+
 
 module.exports = router;
