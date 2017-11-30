85cddc1e47a4d2a8493d1e669f2578ac5b24361b
Tamir <Tamir@DESKTOP-8R67EMV>
2017-11-30 16:17:06 +0200

    # created the post-transfer-page

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,17 @@ var router = express.Router();
 
 
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user });
+    res.render('index', {user: req.user});
 });
 
-router.get('/register', function(req, res) {
-    res.render('register', { });
+router.get('/register', function (req, res) {
+    res.render('register', {});
 });
 
-router.post('/register', function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+router.post('/register', function (req, res) {
+    Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
         if (err) {
-            return res.render('register', { account : account });
+            return res.render('register', {account: account});
         }
 
         passport.authenticate('local')(req, res, function () {
@@ -24,37 +24,38 @@ router.post('/register', function(req, res) {
     });
 });
 
-router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+router.get('/login', function (req, res) {
+    res.render('login', {user: req.user});
 });
 
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/transfer');
-});
-
-
-router.get('/transfer', function(req, res) {
-    res.render('bankTransferForm', { user : req.user });
-});
-
-router.get('/transfer/money', function (req, res) {
-    console.log('account: ' + req.query['accountNum'] + ', amount: ' + req.query['amount']);
-    res.send('Transfer by get');
-});
-
-
-router.post('/transfer', function(req, res) {
-    console.log("amount: " + req.body.amount + ",  account number: " + req.body.accountNum);
-    res.send("Transfer executed by post");
-});
-
-
-router.get('/logout', function(req, res) {
-    req.logout();
+router.post('/login', passport.authenticate('local'), function (req, res) {
     res.redirect('/');
 });
 
+
+router.get('/transfer/get', function (req, res) {
+    res.render('bankTransferGET', {user: req.user});
+});
+
+router.get('/transfer/get/money', function (req, res) {
+    console.log('account: ' + req.query['accountNum'] + ', amount: ' + req.query['amount']);
+    res.send('Transfer by GET');
+});
+
+router.get('/transfer/post', function (req, res) {
+    res.render('bankTransferPOST', {user: req.user});
+});
+
+router.post('/transfer/post/money', function (req, res) {
+    console.log("amount: " + req.body.amount + ",  account number: " + req.body.accountNum);
+    res.send("Transfer by POST");
+});
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
 
 module.exports = router;
