eb33b0625df9531e4f1dea73ec45cf8da1713eee
Eden Shuker <edenshuker1997@gmail.com>
2017-12-05 23:36:08 +0200

    authenticate pages using session id

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,13 @@ router.get('/register', function (req, res) {
 
 
 router.post('/register', function (req, res) {
-
     Account.register(new Account({username: req.body.username}),
         req.body.password, function (err, account) {
             if (err) {
                 return res.render('register', {account: account});
             }
             passport.authenticate('local')(req, res, function () {
-                res.redirect('/');
+                res.redirect('/login');
             });
         });
 });
@@ -34,38 +33,43 @@ router.get('/login', function (req, res) {
 });
 
 router.post('/login', passport.authenticate('local'), function (req, res) {
-    res.redirect('/');
+    var promise = Account.findOne({username: req.user.username}).exec();
+    promise.then(function (user) {
+        console.log( user._id);
+        req.session.user_id = user._id;
+        res.redirect('/');
+    })
 });
 
 
 // regular transfer get
-router.get('/transfer/get', function (req, res) {
+router.get('/transfer/get',checkAuth, function (req, res) {
     res.render('bankTransferGET', {user: req.user});
 });
 
-router.get('/transfer/get/money', function (req, res) {
-    console.log('account: ' + req.query['accountNum'] + ', amount: ' + req.query['amount']);
+router.get('/transfer/get/money',checkAuth, function (req, res) {
+    console.log('account: ' + req.query['accountNum'] + ', amount: ' + req.query['amount'] + " from: " + req.user.username);
     res.send('Transfer by GET');
 });
 
 
 // regular transfer post
-router.get('/transfer/post', function (req, res) {
+router.get('/transfer/post', checkAuth, function (req, res) {
     res.render('bankTransferPOST', {user: req.user});
 });
 
-router.post('/transfer/post/money', function (req, res) {
-    console.log("amount: " + req.body.amount + ",  account number: " + req.body.accountNum);
+router.post('/transfer/post/money',checkAuth, function (req, res) {
+    console.log("amount: " + req.body.amount + ",  account number: " + req.body.accountNum + " from: " + req.user.username);
     res.send("Transfer by POST");
 });
 
 
 // protected transfer get
-router.get('/transfer-protected/get', function (req, res) {
+router.get('/transfer-protected/get',checkAuth, function (req, res) {
     res.render('protectedTransferGET', {user: req.user});
 });
 
-router.get('/transfer-protected/get/money', function (req, res) {
+router.get('/transfer-protected/get/money',checkAuth, function (req, res) {
     var promise = getCsrfTokOf(req.user.username);
     promise.then(function (user) {
         console.log("csrfToker= " + user.csrfToken);
@@ -81,11 +85,11 @@ router.get('/transfer-protected/get/money', function (req, res) {
 
 
 // protected transfer post
-router.get('/transfer-protected/post', function (req, res) {
+router.get('/transfer-protected/post', checkAuth, function (req, res) {
     res.render('protectedTransferPOST', {user: req.user});
 });
 
-router.post('/transfer-protected/post/money', function (req, res) {
+router.post('/transfer-protected/post/money', checkAuth, function (req, res) {
     var promise = getCsrfTokOf(req.user.username);
     promise.then(function (user) {
         console.log("csrfToker= " + user.csrfToken);
@@ -101,15 +105,23 @@ router.post('/transfer-protected/post/money', function (req, res) {
 
 
 // logout
-router.get('/logout', function (req, res) {
+router.get('/logout',checkAuth, function (req, res) {
+    delete req.session.user_id;
     req.logout();
     res.redirect('/');
 });
 
 function getCsrfTokOf(username) {
-    var promise = Account.findOne({ username: username }).exec();
+    var promise = Account.findOne({username: username}).exec();
     return promise;
 }
 
+function checkAuth(req, res, next) {
+    if (!req.session.user_id) {
+        res.send('You are not authorized to view this page');
+    } else {
+        next();
+    }
+}
 
 module.exports = router;
